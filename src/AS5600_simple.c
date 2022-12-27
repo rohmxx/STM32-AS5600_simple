aa38cb169a501937671967f65414dfeca26f555c
Rohman Aditiya <74638607+rohmxx@users.noreply.github.com>
2022-12-27 17:30:36 +0700

    Create AS5600_simple.c

---
+++ src/AS5600_simple.c
@@ -0,0 +1,8 @@
+uint16_t AS5600_RawAngle(){
+	uint8_t angle[2];
+	if(HAL_I2C_Mem_Read(&hi2c2, 0x36<<1, 0x0E, I2C_MEMADD_SIZE_8BIT, angle, 2, 200)!=HAL_OK){
+		return HAL_ERROR;
+    break:
+	}
+	return angle[0]*256+angle[1];
+}
